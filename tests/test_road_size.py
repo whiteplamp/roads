6dcc3589cb9c0a7f085954c5d2468c6f3a67e728
Tokarev Denis <yan.netsky@gmail.com>
2024-11-15 18:29:15 +0300

    Add base structures and test size calculations

---
+++ tests/test_road_size.py
@@ -0,0 +1,48 @@
+import pytest
+
+from src.structures import Point, Road
+
+
+def test_size():
+    points = [
+        Point(0, 1),
+        Point(0, 0),
+        Point(1, 0),
+        Point(1, 1),
+    ]
+
+    results = [
+        {
+            "points": [0, 1],
+            "value": 1
+        },
+        {
+            "points": [0, 2],
+            "value": 1.41
+        },
+        {
+            "points": [0, 3],
+            "value": 1
+        },
+        {
+            "points": [1, 2],
+            "value": 1
+        },
+        {
+            "points": [1, 3],
+            "value": 1.41
+        },
+        {
+            "points": [2, 3],
+            "value": 1
+        },
+    ]
+
+    for result in results:
+        road = Road(
+            a=points[result['points'][0]],
+            b=points[result['points'][1]],
+        )
+        assert round(road.size, 2) == round(result['value'], 2)
+
+
