a7b7ae36b9b32b39f814a131504db3858de60191
Tokarev Denis <yan.netsky@gmail.com>
2024-11-17 16:42:04 +0300

    Initialize vue + electron + tailwind-css project

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-full h-screen">
+        <div class="w-1/3 h-full border-r border-black">
+            <div class="w-full h-full flex flex-col">
+                <div class="m-10 px-3 py-2 rounded-lg font-bold text-white text-sm bg-blue-500 hover:bg-blue-600 transition-all duration-300"
+                     v-on:click="createPoint"
+                >
+                    Создать точку
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const points = ref([])
+
+
+
+
+</script>
--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
-from src.without_optimization.generator import Generator
 from src.without_optimization.visualizer import Visualizer
 
-generator = Generator(points=12, roads=0, map_size_x=800, map_size_y=600)
 
+visualizer = Visualizer()
 
-_map = generator.generate_map_with_all_roads()
-
-print(_map)
-
-
-visualizer = Visualizer(_map=_map)
-
-visualizer.draw()
+visualizer.main()
